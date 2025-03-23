417cd2a23e3662a7a9db197a1de24284eecd526c
secrescence <edselpisuena@gmail.com>
2025-03-23 19:05:32 +0800

    Moved blog link under Projects tab

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
     path("", views.portfolio_home, name="portfolio_home"),
+    path("projects/", views.projects, name="projects"),
+    path("about/", views.about, name="about"),
+    path("blog.html", views.blog_home, name="blog_home"),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def portfolio_home(request):
     return render(request, "portfolio_app/home.html")
+
+
+def projects(request):
+    return render(request, "portfolio_app/projects.html")
+
+
+def about(request):
+    return render(request, "portfolio_app/about.html")
+
+
+def blog_home(request):
+    return render(request, "blog/blog_home.html")
