30ab78074891a0ff7c05274513efb134c4b1bf31
secrescence <edselpisuena@gmail.com>
2025-03-25 23:37:20 +0800

    Added deployment configuration for Railway

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i!q*kzjz6%n$k!*0q@1nlq5_i^0bywz*ajbo410p&=de9pm=0)"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
